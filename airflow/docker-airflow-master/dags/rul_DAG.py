f285ea99b6c7bf6b18100f18526aa485a44a952f
ddealwis09 <dinushdealwis@gmail.com>
2023-01-01 08:46:24 -0500

    added autoML pipeline

--- airflow/docker-airflow-master/dags/rul_DAG.py
+++ airflow/docker-airflow-master/dags/rul_DAG.py
@@ -17,12 +17,30 @@ with DAG(
         },
     description='Predicting Remaining Useful Life of Turbofan Engines',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(2022, 12, 18)
+    start_date=datetime(2022, 12, 21)
 ) as dag:
 
-    # load data to raw data folder
-    get__data = BashOperator(
+    # check raw data file exists
+    t1 = BashOperator(
         task_id='get_data',
+        bash_command="Hello World",
+    )
+
+    # create copy in data/processed with sensor names
+    t2 = BashOperator(
+        task_id='copy_to_processed',
+        bash_command="Hello World",
+    )
+
+    # perform pre-processing and model training
+    t3 = BashOperator(
+        task_id='preprocess',
+        bash_command="Hello World",
+    )
+
+    # send trained model to /models to be used for API calls
+    t4 = BashOperator(
+        task_id='send_model',
         bash_command="Hello World",
     )
 
---
+++ streamlit/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.title("Predicting Turbofan Engine Remaining Useful Life (RUL)")
+    st.text("Machine Learning")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
