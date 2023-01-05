3135b85bf7cb13a743400c3af356f76ec4759d1b
ddealwis09 <dinushdealwis@gmail.com>
2023-01-05 14:58:55 -0500

    added file checks

--- airflow/docker-airflow-master/dags/rul_DAG.py
+++ airflow/docker-airflow-master/dags/rul_DAG.py
@@ -2,10 +2,13 @@ from airflow import DAG
 #from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
+
+# docker-airflow-master % docker-compose -f docker-compose-LocalExecutor.yml up -d
 
 # instantiate DAG
 with DAG(
-    'rul-workflow',
+    'rul-flow',
     default_args={
         'owner': 'Dinush De Alwis', 
         'depends_on_past': False,
@@ -13,34 +16,43 @@ with DAG(
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
-        'retry_delay': timedelta(minutes=5),
+        'retry_delay': timedelta(minutes=1),
         },
     description='Predicting Remaining Useful Life of Turbofan Engines',
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2022, 12, 21)
+    schedule_interval='@daily',
+    catchup=False,
+    start_date=datetime(2023, 1, 4)
 ) as dag:
 
-    # check raw data file exists
+    # check training data exists
     t1 = BashOperator(
-        task_id='get_data',
-        bash_command="Hello World",
+        task_id='check_train_exists',
+        depends_on_past = False, 
+        bash_command='shasum /Users/dinushdealwis/Documents/ML_Projects/RUL/data/raw/train_FD001.txt',
+        retries = 2,
+        retry_delay = timedelta(seconds=15)
     )
 
-    # create copy in data/processed with sensor names
+    # check test feature data exists
     t2 = BashOperator(
-        task_id='copy_to_processed',
-        bash_command="Hello World",
+        task_id='check_test_features',
+        depends_on_past = False, 
+        bash_command='shasum /Users/dinushdealwis/Documents/ML_Projects/RUL/data/raw/test_FD001.txt',
+        retries = 2,
+        retry_delay = timedelta(seconds=15)
     )
 
-    # perform pre-processing and model training
+    # check test feature data exists
     t3 = BashOperator(
-        task_id='preprocess',
-        bash_command="Hello World",
+        task_id='check_test_labels',
+        depends_on_past = False, 
+        bash_command='shasum /Users/dinushdealwis/Documents/ML_Projects/RUL/data/raw/RUL_FD001.txt',
+        retries = 2,
+        retry_delay = timedelta(seconds=15)
     )
 
-    # send trained model to /models to be used for API calls
-    t4 = BashOperator(
-        task_id='send_model',
-        bash_command="Hello World",
-    )
+    [t1,t2,t3]
 
+
+
+    
